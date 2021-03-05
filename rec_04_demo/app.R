5af9011022f8f203d3e28bc3b434df8cf2b092ca
t-cobb <trevorstormcobb@gmail.com>
2021-03-05 17:51:30 -0500

    Recitation sample app

--- rec_04_demo/app.R
+++ rec_04_demo/app.R
@@ -15,7 +15,11 @@ library(primer.data)
 library(shinythemes)
 library(shiny)
 
+# this is the source file / r script that calls in the data.
+# this source() function references that script so I can then do anything
+# I need in this as if it were in the same rmd 
 source(file = "clean_pov_map.R")
+
 
 # for plot opt. 2:
 # CSV had same tibble force issue with [geometry]
@@ -31,26 +35,36 @@ ui <- navbarPage(
                      titlePanel("Qscores Data"),
                      sidebarLayout(
                          sidebarPanel(
-                             selectInput(
-                                 "var_plot",
+                            
+        # selectInput is where interactivity starts. Choices is the label names
+        # when user selects "Voted" returns TRUE or FALSE based on server logic                    
+                              selectInput(
+                                 "rufio",
                                  "Choose a Response Category",
-                                 choices = c("Enrollment" = "enrollment", 
-                                   "Workload per Week" = "hours")
+                                 choices = c("Voted" = 1, 
+                                   "Did not vote" = 0)
                              ),
                              width = 300),
-                         plotOutput("line_plot",
+                         plotOutput("object",
                                     width = 550,
                                     height = 500)))),
-        
+
+        # this is created live in the server, referencing a static data set
+
         tabPanel("Map",
                  titlePanel("Map of % HH Below Poverty Line"),
                  plotOutput("pov_plot2",
                             width = 450,
                             height = 400)),
         
+        # this references the source file 
+        
         tabPanel("Other Map",
                  titlePanel("Another way to bring in a Map"),
                  plotOutput("map2")),
+        
+        #this is just an image, and it needs to be in a www file, 
+        # called in the img function. Loads faster
         
         tabPanel("Working Map",
                  titlePanel("Yet Another way to bring in a Map"),
@@ -79,14 +93,33 @@ ui <- navbarPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+output$object <- renderPlot({
+    
+   # ifelse(input$rufio == "1", 
+   #        z <- 1, 
+   #        z <- 0)
+    
+     cces %>%
+        mutate(vote1 = ifelse(voted == "Voted", 1, 0)) %>%
+        filter(vote1 == input$rufio) %>%
+         
+        ggplot(aes(x = faminc,
+                   y = age)) +
+        geom_col()
+
+})    
+    
     output$link <- renderUI({
         tags$a(href="https://beaumeche.shinyapps.io/Shiny-Recitation-Demo-wk4/", "Here is the link to this repo")
     })
         
     output$line_plot <- renderPlot({
         
-        ifelse(input$var_plot == "enrollment",
-               z <- qscores$enrollment,
+    # this is how you reference the input variable (input$var_plot)
+    # previous plot from Beau's example
+        
+        ifelse(input$var_plot == TRUE,
+               z <- cces$voted,
                z <- qscores$rating)
         
             qscores %>% 
